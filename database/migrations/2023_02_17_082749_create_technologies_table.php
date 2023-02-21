cb5ec1dabb2b122552db819d465d4ddad87f7b7f
Matt <mattsandovalb@gmail.com>
2023-02-21 10:09:35 +0100

    relational tables tech, courses and exercices

--- database/migrations/2023_02_17_082749_create_technologies_table.php
+++ database/migrations/2023_02_17_082749_create_technologies_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description');
             $table->string('tech');
-            $table->string('poster');
-            $table->string('level');
+
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('courses');
+        Schema::dropIfExists('technologies');
     }
 };
