71f50a64a0bf2ec7b8bb2be39c8b1da91173a0e4
OksanaFedyukova <avseniya222@gmail.com>
2023-03-06 12:29:14 +0100

    npm install

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 class CoursesController extends Controller
 {
 
-
+/* 
     public function __construct()
     {  
         $this->middleware('auth:api', ['except' => ['index', 'show']]);
 
-    }
+    } */
     /**
      * Display a listing of the resource.
      *
